043493f9f7d40c2b7f78331c391e58b22dbceed9
zaheer jk <zaheerxaiml@gmail.com>
2025-09-06 02:25:16 +0530

    Major Project Upgrade: Security, API Integration & PostgreSQL Migration”

--- nouproject/urls.py
+++ nouproject/urls.py
@@ -19,8 +19,51 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from nouapp.views import set_theme
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import re_path
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Nalanda Open University LMS API",
+      default_version='v1',
+      description="API documentation for the Nalanda Open University Learning Management System",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@nou.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)
+    path('',include(('nouapp.nouappurls','nouapp'),namespace='nouapp')),
+    path('studentapp/',include(('studentapp.studentappurls','studentapp'),namespace='studentapp')),
+    path('adminapp/',include(('adminapp.adminappurls','adminapp'),namespace='adminapp')),
+    path('set-theme/', set_theme, name='set-theme'),
+]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT), schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/
+    path('',include(('nouapp.nouappurls','nouapp'),namespace='nouapp')),
+    path('studentapp/',include(('studentapp.studentappurls','studentapp'),namespace='studentapp')),
+    path('adminapp/',include(('adminapp.adminappurls','adminapp'),namespace='adminapp')),
+    path('set-theme/', set_theme, name='set-theme'),
+]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT), schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/
+    path('',include(('nouapp.nouappurls','nouapp'),namespace='nouapp')),
+    path('studentapp/',include(('studentapp.studentappurls','studentapp'),namespace='studentapp')),
+    path('adminapp/',include(('adminapp.adminappurls','adminapp'),namespace='adminapp')),
+    path('set-theme/', set_theme, name='set-theme'),
+]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT), schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
+
     path('',include(('nouapp.nouappurls','nouapp'),namespace='nouapp')),
     path('studentapp/',include(('studentapp.studentappurls','studentapp'),namespace='studentapp')),
     path('adminapp/',include(('adminapp.adminappurls','adminapp'),namespace='adminapp')),
